117b83b398b5a3c1950626b5aa79e71a791e9051
Yvan Sraka <yvan.sraka@gmail.com>
2018-03-16 16:51:59 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut children = vec![];
+    let args: Vec<String> = std::env::args().collect();
+    for arg in &args[1..] {
+        let path = arg.clone();
+        children.push(std::thread::spawn(move || -> String {
+            let file = std::fs::File::open(path.clone())
+                .expect(&format!("Error: couldn't open file `{}`", path));
+            let mut reader = std::io::BufReader::new(file);
+            let mut buffer = String::new();
+            loop {
+                let len = reader.read_line(&mut buffer)
+                    .expect(&format!("Error: couldn't read file `{}`", path));
+                if len == 0 {
+                    return buffer;
+                }
+            }
+        }));
+    }
+    for child in children {
+        print!("{}", &child.join().unwrap());
+    }
 }
