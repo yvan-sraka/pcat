b9b75180fce4386936b9916b7343926b83774156
Yvan Sraka <yvan.sraka@gmail.com>
2018-12-14 10:23:01 +0100

    Fix lints given by Clippy

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
         let path = arg.clone();
         children.push(std::thread::spawn(move || -> String {
             let file = std::fs::File::open(path.clone())
-                .expect(&format!("Error: couldn't open file `{}`", path));
+                .unwrap_or_else(|_| panic!("Error: couldn't open file `{}`", path));
             let mut reader = std::io::BufReader::new(file);
             let mut buffer = String::new();
             loop {
                 let len = reader
                     .read_line(&mut buffer)
-                    .expect(&format!("Error: couldn't read file `{}`", path));
+                    .unwrap_or_else(|_| panic!("Error: couldn't read file `{}`", path));
                 if len == 0 {
                     return buffer;
                 }
