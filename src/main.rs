e80d94a2ebaf45e26e5281b8f3b67eec258afdc5
Yvan Sraka <yvan@sraka.xyz>
2021-08-28 14:21:27 +0200

    Update README.md

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! # 🐱 `pcat`
 //!
 //! `pcat` is an alternative version of [`cat`](https://linux.die.net/man/1/cat)
-//! that will not fail (to read inputs) when results of computation to concatenate
+//! that will not fail (to read inputs) when results of computation
 //! are not available in the right order!
 //!
 //! ```shell
@@ -28,8 +28,8 @@
 //! ```
 //!
 //! This could be particularly useful when you want to prototype parallel code
-//! that's rely on concatenation at some point, using classic `cat` would create IO
-//! lock which is not really funny... this project was made at origin for the
+//! that relies on concatenation at some point, using classic `cat` would create IO
+//! lock which is not funny... this project was made at origin for the
 //! purpose of [YeAST](https://github.com/yvan-sraka/YeAST/) implementation!
 
 use std::io::prelude::*;
